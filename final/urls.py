633cdd926df56b55db487b9289fcddee10b05276
ufairah bashir <72339406+ufairahbashir@users.noreply.github.com>
2021-12-06 17:43:05 +0800

    Add files via upload

---
+++ final/urls.py
@@ -0,0 +1,20 @@
+from os import name
+from django.conf.urls import url
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from .views import index_view, detail_view, create_view, update_view, delete_view
+
+app_name = 'todo'
+urlpatterns = [
+    # url untuk halaman daftar task
+    path('', index_view, name='index'),
+    # url untuk halaman detail task
+    path('<int:task_id>', detail_view, name='detail'),
+    # url untuk halaman tambah task
+    path('create', create_view, name='create'),
+    # url untuk halaman ubah task
+    path('update/<int:task_id>', update_view, name='update'),
+    # url untuk menghapus task
+    path('delete/<int:task_id>', delete_view, name='delete'),
+]
